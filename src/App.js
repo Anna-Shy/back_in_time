9d622a1a367754786f5db75ac6a5e95cb7bc9dc8
Anna-Shy <shylkina.ao@gmail.com>
2024-02-07 23:47:15 +0200

    add: react router for page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import { LandingPage } from './pages/landingPage/LandingPage';
+import { NewArrivalsPage } from './pages/newArrivalsPage/NewArrivalsPage';
+import { SalePage } from './pages/salePage/SalePage';
+import { CatalogPage } from './pages/catalogPage/CatalogPage';
+import { ContactPage } from './pages/contactPage/ContactPage';
+import { ErrorPage } from "./pages/errorPage/ErrorPage";
 
 import './styles/global.scss';
 
+const router = createBrowserRouter([
+  {
+    // path: "/back_in_time",
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/new-arrivals",
+    element: <NewArrivalsPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/catalog",
+    element: <CatalogPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/sale",
+    element: <SalePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 function App() {
   return (
-    <LandingPage />
+    <RouterProvider router={router} />
   );
 }
 
