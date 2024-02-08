26b151a58da37dc54717d52d3568d84deee7ba56
Anna-Shy <shylkina.ao@gmail.com>
2024-02-08 22:12:12 +0200

    add: NewArrivalsPage component

--- src/pages/newArrivalsPage/NewArrivalsPage.jsx
+++ src/pages/newArrivalsPage/NewArrivalsPage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import './newArrivalsPage.scss'
+import { Header } from "../../components/header/Header";
+import { Card } from "../../components/card/Card";
+import { Linkin } from "../../components/linkin/Linkin";
+import { Footer } from "../../components/footer/Footer";
+
+import { newArrivalsArray } from "./data";
+
+import "./newArrivalsPage.scss";
 
 export const NewArrivalsPage = () => {
   return (
-    <div>NewArrivalsPage</div>
-  )
-}
+    <div className="newArrivals">
+      <Header icons />
+
+      <main className="newArrivals__main">
+        <h2 className="main__title">оновлення</h2>
+
+        <div className="main__cards">
+          {newArrivalsArray.map((item, key) => (
+            <Card key={key} {...item} />
+          ))}
+        </div>
+
+        <Linkin link={"/catalog"} text={"показати ще"} classStyle="main__btn" />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
