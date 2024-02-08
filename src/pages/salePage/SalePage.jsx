9e608746ce06036422885899e242da009526f435
Anna-Shy <shylkina.ao@gmail.com>
2024-02-08 21:51:04 +0200

    add: Sale Page

--- src/pages/salePage/SalePage.jsx
+++ src/pages/salePage/SalePage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import './salePage.scss'
+import { Header } from "../../components/header/Header";
+import { Card } from "../../components/card/Card";
+import { Linkin } from "../../components/linkin/Linkin";
+import { Footer } from "../../components/footer/Footer";
+
+import { saleArray } from "./data";
+
+import "./salePage.scss";
 
 export const SalePage = () => {
   return (
-    <div>SalePage</div>
-  )
-}
+    <div className="sale">
+      <Header />
+
+      <main className="sale__main">
+        <h2 className="main__title">розпродаж</h2>
+
+        <div className="main__cards">
+          {saleArray.map((item, key) => (
+            <Card key={key} {...item} />
+          ))}
+        </div>
+
+        <Linkin link={"/catalog"} text={"показати ще"} classStyle="main__btn" />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
