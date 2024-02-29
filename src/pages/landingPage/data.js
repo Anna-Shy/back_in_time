4b41ac1bc4dfc3250ed37ab4651a118f29e7c2fb
Anna-Shy <shylkina.ao@gmail.com>
2024-02-29 19:49:26 +0200

    upd: change gender style for Ipone adaptive to Landing Page

--- src/pages/landingPage/data.js
+++ src/pages/landingPage/data.js
@@ -1,6 +1,4 @@
 import about from "../../source/img/landing/about.png";
-import menAdaptive from '../../source/img/landing/gender/men-adaptive.svg'
-import womenAdaptive from '../../source/img/landing/gender/women-adaptive.svg'
 
 import { saleArray } from "../../source/data/sale";
 import { newArrivalsArray } from "../../source/data/newArrivals";
@@ -32,14 +30,6 @@ const genderArray = [
     {
         link: '#',
         image: require("../../source/img/landing/gender/women.png")
-    },
-    {
-        link: "#",
-        image: menAdaptive
-    },
-    {
-        link: '#',
-        image: womenAdaptive
     }
 ]
 
